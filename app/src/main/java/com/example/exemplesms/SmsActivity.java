7eb36c1a18960e73857063ff21b352bb29010c55
Yoann Kergall <yoann.kergall@gmail.com>
2020-04-16 10:10:38 +0200

    ajout permissions pour l'envoi et la reception de SMS

---
+++ app/src/main/java/com/example/exemplesms/SmsActivity.java
@@ -0,0 +1,14 @@
+package com.example.exemplesms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SmsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms);
+    }
+}
